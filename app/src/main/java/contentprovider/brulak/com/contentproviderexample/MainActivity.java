c5aa811aa90eba790150a3d5960f983133cf884a
Chris Brulak <chris.brulak@bnotions.com>
2015-09-12 17:15:36 -0400

    intial commit

---
+++ app/src/main/java/contentprovider/brulak/com/contentproviderexample/MainActivity.java
@@ -0,0 +1,82 @@
+package contentprovider.brulak.com.contentproviderexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
+
+public class MainActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private EditText mEditText;
+    private Button mButton;
+    private NoteListItemAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
+        final Firebase myFirebaseRef = new Firebase("https://blazing-torch-3193.firebaseio.com/");
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mEditText = (EditText) findViewById(R.id.edit_text);
+        mButton = (Button) findViewById(R.id.button);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        mAdapter = new NoteListItemAdapter(this, mRecyclerView);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String note = mEditText.getText().toString();
+                final NoteListItem item = new NoteListItem(note);
+                mAdapter.addItem(item);
+                mEditText.setText("");
+                mLayoutManager.scrollToPosition(0);
+                NoteDAO dao = new NoteDAO(MainActivity.this);
+                dao.save(item);
+
+                Firebase noteRef = myFirebaseRef.child("notes");
+
+                noteRef.setValue(item);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
